a9a44b551e09a10592afd627d67f5df4b0827e09
Hnikar <hnikar.az@proton.me>
2023-11-09 15:45:01 -0700

    refactor the code to be a little cleaner

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from pubsub import pub
 import time
 
 
-def onReceive(packet, interface, ):
+def on_receive(packet, interface, ):
     # print(packet)
     print("-" * 80)
     print({packet.get("decoded").get("portnum")})  # there are ADMIN_APP and TELEMETRY_APP messages
     whoToWho(packet)
     if packet.get("decoded").get("portnum") == "TEXT_MESSAGE_APP":
-        textMessageApp(packet)
+        text_message_app(packet)
 
     elif packet.get('decoded').get('portnum') == 'ADMIN_APP':
         adminApp(packet)
@@ -33,7 +33,7 @@ def whoToWho(packet):  # displays from and to information
     return
 
 
-def textMessageApp(packet): # displays messages sent over the mess
+def text_message_app(packet):  # displays messages sent over the mesh
     print("Message" * 10)
     print(
         f"Message from {str(packet.get('from'))}, To {str(packet.get('to'))} {str(packet.get('decoded').get('text'))}"
@@ -43,11 +43,11 @@ def textMessageApp(packet): # displays messages sent over the mess
     return
 
 
-def adminApp(packet): # manages the ADMINAPP packet
-    frompacket = str(packet.get('fromId'))
-    topacket = str(packet.get('toId'))
-    if frompacket == topacket:
-        print(f"{frompacket} is me!")
+def adminApp(packet):  # manages the ADMIN_APP packet
+    from_packet = str(packet.get('fromId'))
+    to_packet = str(packet.get('toId'))
+    if from_packet == to_packet:
+        print(f"{from_packet} is me!")
     else:
         pass
     print(
@@ -57,7 +57,7 @@ def adminApp(packet): # manages the ADMINAPP packet
     return
 
 
-def telemetryApp(packet): # manages the TELEMETRY_APP packet
+def telemetryApp(packet):  # manages the TELEMETRY_APP packet
     print("telemetryApp function:")
     print(f"TELEMETRY_APP telemetry notes were: {str(packet.get('decoded').get('telemetry'))}")
     return
@@ -66,8 +66,8 @@ def telemetryApp(packet): # manages the TELEMETRY_APP packet
 def main():
     # Subscribe to the given message type This should display received text
     # the below doesn't display telemetry data
-    pub.subscribe(onReceive, "meshtastic.receive")
-    # By default will try to find a meshtastic device, otherwise provide a device path like /dev/ttyUSB0
+    pub.subscribe(on_receive, "meshtastic.receive")
+    # By default, will try to find a meshtastic device, otherwise provide a device path like /dev/ttyUSB0
     interface = meshtastic.serial_interface.SerialInterface()
 
     # Main application "logic" - This holds the program open to maintain a connection with the radio. This function
